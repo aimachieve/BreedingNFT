3aefd83e844aa399f159b3cc39fee1b5ecb716cc
aimachieve <aim1005achieve@gmail.com>
2022-02-14 20:29:53 +0500

    first commit

--- src/utils/getRpcUrl.js
+++ src/utils/getRpcUrl.js
@@ -1,8 +1,8 @@
 import sample from 'lodash/sample';
 
-if (!process.env.REACT_APP_NODE_1 || !process.env.REACT_APP_NODE_2 || !process.env.REACT_APP_NODE_3) {
-  throw Error('One base RPC URL is undefined');
-}
+// if (!process.env.REACT_APP_NODE_1 || !process.env.REACT_APP_NODE_2 || !process.env.REACT_APP_NODE_3) {
+//   throw Error('One base RPC URL is undefined');
+// }
 
 // Array of available nodes to connect to
 // export const nodes = [process.env.REACT_APP_NODE_1, process.env.REACT_APP_NODE_2, process.env.REACT_APP_NODE_3]
